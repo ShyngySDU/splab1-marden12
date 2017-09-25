6f0c92a755905a4c7f3b681b585d9dfa90a4615b
Dayana Tezekbaeva <mardentsk@gmail.com>
2017-09-25 20:50:02 +0600

    A is DONE

--- sincos.c
+++ sincos.c
@@ -1,10 +1,52 @@
-//Fullname: Vasya Pupkin
-//Group: 3EN04X
+//Fullname: Tezekbayeva Dayana
+//Group: 3EN04B
 
 #include <stdio.h>
 #include <math.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char*argv[]){
-    printf("DAYABABAB")
+int main(int argc, char **argv) {
+    // insert code here...
+    double val = M_PI / 180;
+    int type;
+    int type2;
+    int index;
+    double sinus,cosinus;
+    int sflg = 0,cflg = 0;
+    while ((type = getopt(argc, argv, "s:c:")) !=-1) {
+        switch (type) {
+            case 's':
+                sflg = 1;
+                float convertingSin = atof(optarg);
+                sinus = sin(convertingSin*val);
+                printf("%f\n",sinus);
+                break;
+            case 'c':
+                cflg = 1;
+                float convertingCos = atof(optarg);
+                cosinus = cos(convertingCos*val);
+                printf("%f\n",cosinus);
+                break;
+            case '?':
+                break;
+                
+            default:
+                fprintf(stderr,"getopt");
+                break;
+        }
+        
+    }
+    for (index = optind; index < argc; index++){
+        float convertingSinCos = atof(argv[index]);
+        sinus = sin(convertingSinCos*val);
+        cosinus = cos(convertingSinCos*val);
+        printf("%f\n%f\n",cosinus,sinus);
+    }
     return 0;
 }
+
+
+
