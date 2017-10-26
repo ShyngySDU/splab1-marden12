dcc9c4d50272f100cf56a6fcd35ccb7b76cd0e1f
marden12 <mardentdk@gmail.com>
2017-10-26 09:35:03 +0600

    Update sincos.c

--- sincos.c
+++ sincos.c
@@ -2,51 +2,33 @@
 //Group: 3EN04B
 
 #include <stdio.h>
-#include <math.h>
-#include <getopt.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-    // insert code here...
-    double val = M_PI / 180;
-    int type;
-    int type2;
-    int index;
-    double sinus,cosinus;
-    int sflg = 0,cflg = 0;
-    while ((type = getopt(argc, argv, "s:c:")) !=-1) {
-        switch (type) {
-            case 's':
-                sflg = 1;
-                float convertingSin = atof(optarg);
-                sinus = sin(convertingSin*val);
-                printf("%f\n",sinus);
-                break;
-            case 'c':
-                cflg = 1;
-                float convertingCos = atof(optarg);
-                cosinus = cos(convertingCos*val);
-                printf("%f\n",cosinus);
-                break;
-            case '?':
-                break;
-                
-            default:
-                fprintf(stderr,"getopt");
-                break;
+int c, i;
+double angle;
+
+int main(int argc, char*argv[]) {
+    
+    for (i = 1; i < argc; i++)
+        if (i == 1 && !strcmp(argv[1], "-s")) {
+            angle = atof(argv[2]);
+            printf("%f\n",sin(angle));
+            break;
+        } else if (i == 1 && !strcmp(argv[1], "-c")) {
+               angle = atof(argv[2]);
+                printf("%f\n",cos(angle));
+            break;
+               }
+        else {
+            angle = atof(argv[1]);
+            printf("%f\n",sin(angle));
+            printf("%f\n",cos(angle));
+            break;
         }
-        
-    }
-    for (index = optind; index < argc; index++){
-        float convertingSinCos = atof(argv[index]);
-        sinus = sin(convertingSinCos*val);
-        cosinus = cos(convertingSinCos*val);
-        printf("%f\n%f\n",cosinus,sinus);
-    }
     return 0;
 }
-
-
 
